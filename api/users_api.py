b39329edabb3f579de8b0e26dd00c8dc100b74aa
romanovmaksim <massib796@gmail.com>
2024-01-02 18:31:12 +0200

    architecture commit

---
+++ api/users_api.py
@@ -0,0 +1,4 @@
+from scr.loader import app
+
+@app.get("/api/users/show")
+def show_users():
---
+++ database/postgres.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+# Connect to your postgres DB
+conn = psycopg2.connect("dbname=test user=postgres")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a query
+cur.execute("SELECT * FROM my_data")
+
+# Retrieve query results
+records = cur.fetchall()#TODO: данек))))0)
